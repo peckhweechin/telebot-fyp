ccc0a3ffb25007a3325ff66ab7c5c7a983b717c2
xbai09 <23017049@myrp.edu.sg>
2026-02-02 11:53:00 +0800

    Fix MySQL database env variable name

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const db = mysql.createPool({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQL_DATABASE,
+  database: process.env.MYSQLDATABASE,
   port: process.env.MYSQLPORT,
   waitForConnections: true,
   connectionLimit: 10,
